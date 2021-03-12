4fea1540ad172f44ccf7acf66601d3e3280e43a3
Rocio Fernandez <rociofc0312@gmail.com>
2021-03-12 11:35:48 -0500

    including in itemlistcontainer

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import React, { useState } from 'react';
-import ItemCount from './components/ItemCount/ItemCount';
+import React from 'react';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from './components/Navbar/Navbar';
 import './App.css';
 
-const App = () => {
-
-  const [stock, setStock] = useState(5)
-
-  const decreaseStock = (quantitySelected) => {
-    if (quantitySelected <= stock) {
-      setStock(stock - quantitySelected)
-    } else{
-      alert('La cantidad elegida supera el stock disponible')
-    }
-  }
-
-  return (
-    <div className="App">
-      <NavBar />
-      <ItemListContainer greeting="Welcome to the Fun Shop" />
-      <ItemCount stock={stock} initial={1} onAdd={decreaseStock} />
-    </div>
-  )
-}
+const App = () => (
+  <div className="App">
+    <NavBar />
+    <ItemListContainer greeting="Welcome to the Fun Shop" />
+  </div>
+)
 
 export default App;
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,15 +1,31 @@
-import './styles.css'
+import ItemCount from '../ItemCount/ItemCount';
+import React, { useState } from 'react';
 import slider from '../../assets/slider.png'
+import './styles.css'
 
-const ItemListContainer = ({ greeting }) => (
-    <div className="greeting">
-        <div className="slider">
-            {/* <p className="arrow left">❮</p> */}
-            <img className="image" src={slider} alt="slider"></img>
-            {/* <p className="arrow right">❯</p> */}
+const ItemListContainer = ({ greeting }) => {
+
+    const [stock, setStock] = useState(5)
+
+    const decreaseStock = (quantitySelected) => {
+        if (quantitySelected <= stock) {
+            setStock(stock - quantitySelected)
+        } else {
+            alert('La cantidad elegida supera el stock disponible')
+        }
+    }
+
+    return (
+        <div className="greeting">
+            <div className="slider">
+                {/* <p className="arrow left">❮</p> */}
+                <img className="image" src={slider} alt="slider"></img>
+                {/* <p className="arrow right">❯</p> */}
+            </div>
+            <p>{greeting}</p>
+            <ItemCount stock={stock} initial={1} onAdd={decreaseStock} />
         </div>
-        <p>{greeting}</p>
-    </div>
-)
+    )
+}
 
 export default ItemListContainer;
